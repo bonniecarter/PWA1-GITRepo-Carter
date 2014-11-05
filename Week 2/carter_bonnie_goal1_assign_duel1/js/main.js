d382ef4cf9551fd0060c88e04501f2ca55853df1
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-11-05 10:15:00 -0500

    fixed code duel: bonnie carter

--- Week 2/carter_bonnie_goal1_assign_duel1/js/main.js
+++ Week 2/carter_bonnie_goal1_assign_duel1/js/main.js
@@ -1,9 +1,9 @@
 /**
  * Duel Fight Game - Bonnie Carter
- * Date: 4/09/13
+ * Date: 11/5/14
 
  Assignment 1
- Part 1/3 of series
+ Part 2/3 of series
  Updated by Bonnie Carter
 */
 
@@ -11,44 +11,55 @@
 (function(){
 
     console.log("FIGHT!!!");                 //Comment: Display to Browser "Fight"
+
+    var fighter1=["Spiderman",20,100];       //Defining Array for Spiderman #1
+    var fighter2=["Superman",20,100];        //Defining Array for Superman #2
+
+    var round = 1;                           //Defining Global Variable for Fight
+
     console.log ("Spiderman & Superman");    //Comment: Display Battle Names
 
     //player name
-    var playerOneName = "Spiderman";         //Name Player One: Spiderman
-    var playerTwoName = "Superman";          //Name Player Two: Superman
+    //var playerOneName = "Spiderman";         //Name Player One: Spiderman
+    //var playerTwoName = "Superman";          //Name Player Two: Superman
 
     //player damage
-    var player1Damage = 20;                  //Damage to be reduced
-    var player2Damage = 20;                  //Damage to be reduced
+    //var player1Damage = 20;                  //Damage to be reduced
+    //var player2Damage = 20;                  //Damage to be reduced
 
     //player health
-    var playerOneHealth = 100;               //Start Player One Health 100
-    var playerTwoHealth = 100;               //Start Player Two Health 100
+    //var playerOneHealth = 100;               //Start Player One Health 100
+    //var playerTwoHealth = 100;               //Start Player Two Health 100
 
     //initiate round
-    var round=0;                             //Variable Round Starts @ 0
+    //var round=0;                             //Variable Round Starts @ 0
                                              //function fight starts with an alert
     function fight(){
-        alert(playerOneName+":"+playerOneHealth+"  *START*  "+playerTwoName+":"+playerTwoHealth);
+
+        console.log("FIGHT!!!");
+
+        //Main Pop-Up for Fighting Game
+        alert(fighter1[0]+":"+fighter1[2]+"  *START*  "+fighter2[0]+":"+fighter2[2]);
                                              //pop up with player one name and health and player two health
         for (var i = 0; i < 10; i++)
                                              //incrementing by one
         {
                                              //random formula is - Math.floor(Math.random() * (max - min) + min);
                                              //min damage .5 (random)
-            var minDamage1 = player1Damage * .5;
+            var minDamage1 = fighter1[1] * .5;
                                              //min damage .5 (random)
-            var minDamage2 = player2Damage * .5;
+            var minDamage2 = fighter2[1] * .5;
                                              //A Number, representing a number from 0 up to but not including 1
                                              //The Math.floor(x) function returns the largest integer less than or equal to a number "x".
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+            var f1 = Math.floor(Math.random()*(fighter1[1]-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(fighter2[1]-minDamage2)+minDamage2);
 
                                              //inflict damage
-            playerOneHealth-=f1;
-            playerTwoHealth-=f2;
+            fighter1[2]-=f1;
+            fighter2[2]-=f2;
                                              //console log: player 1 name and the health  player 2 name and the health.
-            console.log(playerOneName+": "+playerOneHealth + " " + playerTwoName+":"+playerTwoHealth);
+            //console.log(playerOneName+": "+playerOneHealth + " " + playerTwoName+":"+playerTwoHealth);
+            console.log(fighter1[2]+" "+fighter2[2]);
 
                                              //check for victor (winner) assign function winnerCheck to declared variable result
             var result = winnerCheck();
@@ -57,7 +68,7 @@
             if (result==="no winner")
             {
                 round++;
-                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+                alert(fighter1[0]+":"+fighter1[2]+"  *ROUND "+round+" OVER"+"*  "+fighter2[0]+":"+fighter2[2]);
 
             } else{
                 alert(result);
@@ -70,14 +81,14 @@
                                              //if player 1 or 2 has les then 1 the opposite player "wins" then return result to variable
     function winnerCheck(){
         var result="no winner";
-        if (playerOneHealth<1 && playerTwoHealth<1)
+        if (fighter1[2]<1 && fighter2[2]<1)
         {
             result = "You Both Die";
-        } else if(playerOneHealth<1){
-            result =playerTwoName+" WINS!!!"
-        } else if (playerTwoHealth<1)
+        } else if(fighter1[2]<1){
+            result =fighter2[0]+" WINS!!!"
+        } else if (fighter2[2]<1)
         {
-            result = playerOneName+" WINS!!!"
+            result = fighter1[0]+" WINS!!!"
         };
        return result;
     };

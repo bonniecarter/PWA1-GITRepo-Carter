ee906c69bdb995326b7ff73535e9ac2cd96a6173
bonniecarter <bonniecarter@Bonnies-MacBook-Pro.local>
2014-11-18 12:20:03 -0500

    week 4 files: bonnie carter

---
+++ Week 4/goal7_assign_objects/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ * NAME: BONNIE J CARTER
+ */
+
---
+++ Week 4/goal7_assign_objects/js/person.js
@@ -0,0 +1,6 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ * NAME: BONNIE J CARTER
+ */
